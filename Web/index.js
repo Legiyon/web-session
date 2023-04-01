3da5ec15ed10545eb0d41fb215f9e39881b9a32b
Legiyon <munikarnewx9@gmail.com>
2023-04-01 22:47:42 +0545

    changes

---
+++ Web/index.js
@@ -0,0 +1,11 @@
+//variable declare and assign
+
+
+let personName = 'hari shyam';
+let number = 60;
+const nio = `${personName} ${pers}`
+
+
+
+console.log(typeof (personName));
+console.log(person)
